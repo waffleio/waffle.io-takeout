273328f2f37eb91e0bbf3c1651a6b5b6d07d4fdf
Andrew Homeyer <homeyer@gmail.com>
2015-04-22 18:59:36 -0600

    add volume mount when running docker for customer CA certificates

---
+++ upload.sh
@@ -0,0 +1,20 @@
+file=$1
+echo "Uploading $file to S3"
+
+bucket="waffleio-takeout"
+resource="/${bucket}/${file}"
+contentType="application/zip"
+dateValue=`TZ=utc date "+%a, %d %h %Y %T %z"`
+stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+s3Key=$WAFFLE_AWS_ACCESS_KEY_ID
+s3Secret=$WAFFLE_AWS_SECRET_ACCESS_KEY
+signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+
+curl \
+  -k -X PUT -T "${file}" \
+  -H "Host: ${bucket}.s3.amazonaws.com" \
+  -H "Date: ${dateValue}" \
+  -H "Content-Type: ${contentType}" \
+  -H "Authorization: AWS ${s3Key}:${signature}" \
+  https://${bucket}.s3.amazonaws.com/${file} \
+  | echo
